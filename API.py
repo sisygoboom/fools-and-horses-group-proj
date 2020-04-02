38ba15af10ca0af26f0c0a516f92ded15e54f9c0
sisygoboom <chrisgraneyward@gmail.com>
2020-04-02 20:09:59 +0100

    Update GUI
    Adapted API to accept AJAX requests and modified GUI to send ajax request instead of form post request for dynamic, single-page response representation.

--- API.py
+++ API.py
@@ -1,11 +1,14 @@
 import numpy as np 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import modelLoader as fl
 
 
 load = fl.fileManager()
 #create the flask app
 app= Flask(__name__)
+
+cors = CORS(app)
 
 #load th previously trained model from file 
 model = load.loadIt("./Models/version2.gz")
@@ -22,8 +25,10 @@ def predict_genre():
         pred = model.predict(movie_plot)
         #convert from numpy array to list 
         pred = pred.tolist()
+        response = jsonify('genre_prediction', pred)
+        response.headers.add('Access-Control-Allow-Origin', '*')
         #send json result
-        return jsonify('genre_prediction', pred)
+        return response
 
 #this is only used while in development, remove later
 if __name__ == "__main__":
